f4f22548bb3fd737c3d7dfa0cde80f8a19a66e8c
Matheus Oliveira <teteuoliveira12@gmail.com>
2021-06-08 17:54:34 -0300

    Enviando Codigo

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    private CharacterController controller;
+
+    public float speed;
+    public float jumpHeigth;
+    private float jumpVelocity;
+    public float gravity;
+
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+    }
+
+    void Update()
+    {
+
+        Vector3 direction = Vector3.forward * speed;
+        
+        if ( controller.isGrounded ) {
+            if ( Input.GetKeyDown( KeyCode.Space ) ) {
+                jumpVelocity = jumpHeigth;
+            }
+        } else {
+            jumpVelocity -= gravity;
+        }
+
+        direction.y = jumpVelocity;
+
+        controller.Move( direction * Time.deltaTime );
+
+    }
+}
