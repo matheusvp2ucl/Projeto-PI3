8e097b9d98fd8de00ab753dd512f352a7128e099
Matheus Oliveira <teteuoliveira12@gmail.com>
2021-06-09 20:33:39 -0300

    Inicial

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,90 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    private CharacterController controller;
+
+    public float speed;
+    public float jumpHeigth;
+    private float jumpVelocity;
+    public float gravity;
+
+    public float rayRadios;
+    public LayerMask layer;
+
+    public float HSpeed;
+
+    private int blockMoveRight;
+    private int blockMoveLeft;
+    private bool isMovingRight;
+    private bool isMovingLeft;
+
+
+    void Start() {
+        controller = GetComponent<CharacterController>();
+    }
+
+    void Update() {
+
+        Vector3 direction = Vector3.forward * speed;
+
+        if (controller.isGrounded) {
+
+            if (Input.GetKeyDown(KeyCode.Space)) {
+                jumpVelocity = jumpHeigth;
+            }
+
+            if (Input.GetKeyDown(KeyCode.RightArrow) && blockMoveRight != 1 && !isMovingRight) {
+                isMovingRight = true;
+                StartCoroutine(RightMove());
+                blockMoveRight++;
+                blockMoveLeft--;
+            }
+
+            if (Input.GetKeyDown(KeyCode.LeftArrow) && blockMoveLeft != 1 && !isMovingLeft) {
+                isMovingLeft = true;
+                StartCoroutine(LeftMove());
+                blockMoveLeft++;
+                blockMoveRight--;
+            }
+
+        } else {
+            jumpVelocity -= gravity;
+        }
+
+        OnCollision();
+
+        direction.y = jumpVelocity;
+
+        // controller.Move(direction * Time.deltaTime);
+
+    }
+
+    IEnumerator LeftMove() {
+        for (float i = 0; i < 10; i += 0.1f) {
+            controller.Move(Vector3.left * Time.deltaTime * HSpeed);
+            yield return null;
+        }
+        isMovingLeft = false;
+    }
+
+    IEnumerator RightMove() {
+        for (float i = 0; i < 10; i += 0.1f) {
+            controller.Move(Vector3.right * Time.deltaTime * HSpeed);
+            yield return null;
+        }
+        isMovingRight = false;
+    }
+
+    void OnCollision() {
+        RaycastHit hit;
+
+        if ( Physics.Raycast( transform.position, transform.TransformDirection(Vector3.forward), out hit, rayRadios, layer) ) {
+            // Game Over
+
+        }
+    }
+
+}
---
+++ Assets/Scripts/SpwnPlataform.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpwnPlataform : MonoBehaviour
+{
+
+    public List<GameObject> platforms = new List<GameObject>();
+    public List<Transform> currentPlatforms = new List<Transform>();
+
+    public int offset;
+
+    private Transform player;
+    private Transform currentPlatformPoint;
+    private int platformIndex;
+
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+
+        for ( int i = 0; i < platforms.Count; i++ ) {
+            Transform p = Instantiate( platforms[i], new Vector3( 0, 0, i * 86 ), transform.rotation ).transform;
+            currentPlatforms.Add( p );
+            offset += 86;
+        }
+
+        currentPlatformPoint = currentPlatforms[platformIndex].GetComponent<Platform>().point;
+
+    }
+
+    void Update()
+    {
+        float distance = player.position.z - currentPlatformPoint.position.z;
+
+        if ( distance >= 5 ) {
+            Recycle(currentPlatforms[platformIndex].gameObject);
+            platformIndex++;
+
+            if (platformIndex > currentPlatforms.Count - 1) {
+                platformIndex = 0;
+            }
+
+            currentPlatformPoint = currentPlatforms[platformIndex].GetComponent<Platform>().point;
+
+        }
+        
+    }
+
+    public void Recycle( GameObject platform ) {
+        platform.transform.position = new Vector3( 0, 0, offset );
+        offset += 86;
+    }
+
+}
